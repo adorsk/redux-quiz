5ce29ffcec479caa3a419f99c2c2167b0e980f89
adorsk <adorsk@gmail.com>
2015-11-30 20:54:10 -0500

    add views dir

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { IndexRoute, Route } from 'react-router';
 
 import AppContainer from '../containers/AppContainer';
-import IndexContainer from '../containers/IndexContainer';
+import IndexView from '../views/IndexView';
 
 
 export default (
   <Route path="/" component={AppContainer}>
-    <IndexRoute component={IndexContainer}/>
+    <IndexRoute component={IndexView}/>
   </Route>
 );
